d385a5e62b742ab0b2c86449f4369ff34ea6f1ee
TC5022 <tanyac5022002@gmail.com>
2022-04-01 17:28:11 -0700

    add contollers and routers

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express');
+import express from 'express';
+import routes from './src/routes/index.js';
 
 const app = express();
 const port = 8000;
+
+app.use('/', routes);
 
 app.listen(port, function(err){
     if(err){
